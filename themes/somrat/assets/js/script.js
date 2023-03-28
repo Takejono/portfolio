2b09e6ebd88c2698c838179bcc7279d665e363b7
takej <takeshi.jonoo@gmail.com>
2023-03-28 19:12:55 +0200

    waypoint change : about

--- themes/somrat/assets/js/script.js
+++ themes/somrat/assets/js/script.js
@@ -189,7 +189,7 @@
 		}
 	});
 
-	$('.portfolio').waypoint({
+	$('.about').waypoint({
 		handler: function (direction) {
 			// Changing Menu background after leaving Header Section
 			$(".menu-container").toggleClass("menu-normal");
